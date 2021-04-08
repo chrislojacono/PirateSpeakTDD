8bea60d103d18a2abf6f4f64df553092f41588a2
Chris LoJacono <clojacono92@gmail.com>
2021-04-08 08:29:54 -0500

    fact setup

---
+++ PirateSpeakTDD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PirateSpeakTDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
