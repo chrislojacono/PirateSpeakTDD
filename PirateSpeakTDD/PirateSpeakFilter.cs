ead1fcdfcf45bf5ecbdb6a15f0a90f8b3b79e3a8
Chris LoJacono <clojacono92@gmail.com>
2021-04-08 08:48:35 -0500

    tests passing

--- PirateSpeakTDD/PirateSpeakFilter.cs
+++ PirateSpeakTDD/PirateSpeakFilter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace PirateSpeakTDD
@@ -9,7 +10,24 @@ namespace PirateSpeakTDD
 
         public List<string> Translator(string jumbleWord, List<string> possibleWords)
         {
+            var matchedList = new List<string>();
 
+            foreach (var word in possibleWords) { 
+           
+                var sortedJumble = string.Concat(jumbleWord.OrderBy(c => c));
+                var sortedTest = string.Concat(word.OrderBy(c => c));
+
+                if (sortedJumble == sortedTest)
+                {
+                    matchedList.Add(word);
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+
+            return matchedList;
         }
     }
 }
